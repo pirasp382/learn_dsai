ab479fbaca7bccd97fc8ec452257e8abe1989b96
Tim Deimel <t.deimel@teckpro.de>
2024-11-18 21:08:25 +0100

    add pension prognose to the service

--- backend/src/services/prediction.py
+++ backend/src/services/prediction.py
@@ -1,5 +1,6 @@
 import pickle
 from src.dto.prediction_Input import Prediction_Input
+from copy import copy
 
 filename="./resources/model.learn"
 file=open(filename, "rb")
@@ -7,3 +8,13 @@ model=pickle.load(file)
 
 def predict(input: Prediction_Input):
     return model.predict([input.toList()])
+
+def pension_prediction(input: Prediction_Input)->list:
+    years = 65-input.age
+    money_year={}
+    for year in range(years):
+        temp = copy(input)
+        temp.age=input.age+year
+        temp.experience=input.experience+year
+        money_year.update({year: int(predict(temp)[0])})
+    return money_year
