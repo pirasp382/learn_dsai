f4f0da8e66b192b7f9fc0e29b120575f9444a3b6
Tim Deimel <t.deimel@teckpro.de>
2024-11-18 20:31:55 +0100

    edit data files

---
+++ frontend/src/data/education.js
@@ -0,0 +1,10 @@
+const educationLevel = [
+    {id: 0, name: "Bachelor's Degree"},
+    {id: 1, name: "Master's Degree"},
+    {id: 2, name: "PhD"},
+    {id: 3, name: "Bachelor's"},
+    {id: 4, name: "High School"},
+    {id: 5, name: "Master's"},
+    {id: 6, name: "phD"},
+]
+export default educationLevel;
---
+++ frontend/src/data/gender.js
@@ -0,0 +1,7 @@
+const genderList=[
+    {id: 0, name:"male"},
+    {id: 1, name: "female"},
+    {id: 2, name: "others"}
+]
+
+export default genderList;
