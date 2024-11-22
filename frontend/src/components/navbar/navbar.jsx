ff55bcdc2f760503f425cdd16b382c40ee2245f5
Tim Deimel <t.deimel@teckpro.de>
2024-11-22 11:43:27 +0100

    #19 navbar bearbeiten

--- frontend/src/components/navbar/navbar.jsx
+++ frontend/src/components/navbar/navbar.jsx
@@ -1,18 +1,19 @@
 import {Link} from "react-router-dom";
 
 
-function Navbar(){
+function Navbar() {
 
-    return <nav>
-        <ul>
-            <li>
-                <Link to={"/"}>Home</Link>
-            </li>
-            <li>
-                <Link to={"/salary"}>Salary</Link>
-            </li>
-        </ul>
-    </nav>
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to={"/"}>Home</Link>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    )
 }
 
 export default Navbar;
